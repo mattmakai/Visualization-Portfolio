878a8def750f0987ffa11c41c103fbddc5fae3d4
Matthew Makai <makaimc@gmail.com>
2012-11-10 14:31:05 -0500

    Updating visualizations with titles.

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'landing', name='landing_page'),
-    url(r'^(?P<slug>[a-zA-Z0-9\-]+)/$', 'visualization', 
+    url(r'^visualization/(?P<slug>[a-zA-Z0-9\-]+)/$', 'visualization', 
         name='visualization'),
 )
